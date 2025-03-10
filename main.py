e694d6b01ca229548836ca4d9f13fb515d3654de
Roan Kirwin <kirwinr@mkmorse.com>
2025-03-10 17:52:50 -0400

    Remove reload option from uvicorn run command in main.py

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ app.include_router(router)
 # uvicorn main:app --host 0.0.0.0 --port 8000
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
